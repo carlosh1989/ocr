ff95e490ae8b2f1b420a6654f6a914523aea59a3
carlos silva <carloshsilva1989@gmail.com>
2021-12-27 15:19:09 -0500

    init

---
+++ app/Http/Controllers/OcrController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OcrController extends Controller
+{
+    public function upload()
+    {
+      return "test";
+    }
+}
